7f2ea1d20b551f474727560fbbfc8f1f74b7a225
Ryan Wincey <ryan.wincey.ctr@nsoc.health.mil>
2016-10-03 16:14:48 -0400

    Updated version

--- Client/src/pwnbrew/network/control/messages/Reload.java
+++ Client/src/pwnbrew/network/control/messages/Reload.java
@@ -49,7 +49,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import pwnbrew.log.RemoteLog;
 import pwnbrew.manager.PortManager;
 
