60ec69f2441b4e5a06bb1e002568443de58eb824
Brandon Peterson <bpeterson926@gmail.com>
2017-10-18 12:33:26 -0400

    minor fix for index out of bound
    hexStringToByteArray would fail on odd string lengths. It will now skip over the last byte of an odd length string, which really shouldnt be passed to it anyway.

--- Server/src/pwnbrew/utilities/SocketUtilities.java
+++ Server/src/pwnbrew/utilities/SocketUtilities.java
@@ -270,7 +270,7 @@ final public class SocketUtilities {
         int len = s.length();
         byte[] data = new byte[len / 2];
 
-        for (int i = 0; i < len; i += 2) {
+        for (int i = 0; (i+1) < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
                                 + Character.digit(s.charAt(i+1), 16));
         }
